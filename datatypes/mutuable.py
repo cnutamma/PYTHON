61666ede95be816c4d666aac6b17162a9750002c
SRINIVASAREDDY TAMMA <tammasrinivasareddz@gmail.com>
2023-07-31 11:39:07 +0530

    ok

---
+++ datatypes/mutuable.py
@@ -0,0 +1,21 @@
+l=[10,20,30]
+s={10,"srinu"}
+d={10:1,20:2}
+
+ba=bytearray()
+
+
+print(type(l))
+print(type(s))
+print(type(d))
+print(type(ba))
+
+'''
+Mutuable objects:
+list
+set
+dictonary(dict)
+bytearray
+
+
+'''
